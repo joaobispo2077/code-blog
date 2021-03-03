481adede143c81c3b5416b8a554105fe62ea7d17
João Bispo <vitor.vi10@hotmail.com>
2021-03-02 21:09:22 -0300

    chore: added user auth strategy

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const { auth } = require('./src/usuarios');
+
 app.use(
   bodyParser.urlencoded({
     extended: true
---
+++ src/usuarios/auth.js
@@ -0,0 +1,36 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+
+const Usuario = require('./usuarios-modelo');
+const { InvalidArgumentError } = require('../erros');
+
+const userAlreadyExists = (user) => {
+  if (!user) throw new InvalidArgumentError("Usuário inexistente.");
+}
+
+const verifyPassword = async (password, hashPassword) => {
+  const isValidPassword = await bcrypt.compare(password, hashPassword);
+  if (!isValidPassword) throw new InvalidArgumentError("E-mail ou senha inválidos");
+}
+
+passport.use(
+  new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'senha',
+    session: false,
+  }),
+  async (email, senha, done) => {
+    try {
+      const usuario = await Usuario.buscaPorEmail(email);
+      userAlreadyExists(usuario);
+
+      await verifyPassword(senha, usuario.hashPassword);
+
+      done(null, usuario);
+    }
+    catch (err) {
+      done(err);
+    }
+  }
+)
